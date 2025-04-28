ce23b822920fb9d04cf074058ee7fda565d94bde
AmalKrishnaKP <amalkrishnakp04@gmail.com>
2025-04-28 22:31:46 +0530

    backend updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-
+import cors from 'cors'
 
 import authRouters from './routers/auth.router.js'
 import messgRouters from './routers/messg.router.js'
@@ -12,6 +12,10 @@ const app=express()
 dotenv.config() // to get the leys from the .env file like PORT,MONGOBD_URI etc..
 
 const PORT=process.env.PORT
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }));
 
 app.use(express.json()) // to extract "json data" from the body
 app.use(cookieParser())// to  reade the cookie from the client
@@ -19,6 +23,9 @@ app.use(cookieParser())// to  reade the cookie from the client
 app.use("/api/auth",authRouters)
 app.use("/api/mesg",messgRouters)
 
+app.get("/one", async(req,res)=>{
+  res.status(200).json({message:"hello"})
+})
 app.listen(PORT,()=>{
     console.log("server listening in port:"+PORT);
     connectDB()
