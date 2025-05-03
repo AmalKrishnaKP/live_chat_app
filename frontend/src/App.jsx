808e73f8ca01bfd764df286b9c62f47036d98429
AmalKrishnaKP <amalkrishnakp04@gmail.com>
2025-05-03 14:41:04 +0530

    navigation updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 
 import HomePage from './pages/HomePage'
@@ -18,6 +18,9 @@ export default function App() {
   useEffect(()=>{
     checkAuth()
   },[checkAuth])
+
+  console.log(authUser);
+  
   
   if (ischeckingAuth && !authUser)return(
     <div className='flex justify-center items-center h-screen'>
@@ -31,11 +34,11 @@ export default function App() {
       <Navbar/> 
       
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/' element={authUser?<HomePage/>: <Navigate to="/login"/>}/>
+        <Route path='/login' element={!authUser?<LoginPage/>:<Navigate to="/"/>}/>
         <Route path='/settings' element={<SettingsPage/>}/>
-        <Route path='/signup' element={<SignUpPage/>}/>
-        <Route path='/profile' element={<ProfilePage/>}/>
+        <Route path='/signup' element={!authUser?<SignUpPage/>:<Navigate to="/"/>}/>
+        <Route path='/profile'  element={authUser?<ProfilePage/>: <Navigate to="/login"/>}/>
       </Routes>
     </div>
   )
